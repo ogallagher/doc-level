1b6d5798cf4c0791c523838758ede82e1c677c72
Owen Gallagher <owengall@icloud.com>
2025-02-17 10:31:51 -0500

    write exportLibrary chunks to render file in order #23

--- src/main.js
+++ src/main.js
@@ -619,14 +619,19 @@ export async function main(argSrc, pagePrev = -1, storyPrev, cycle=true) {
     const renderPath = path.join(args.rendersDir, renderFilename)
     const renderFile = await writer.openFile(renderPath)
 
+    /**
+     * Chain of promises to write to the render file.
+     */
+    let p = Promise.resolve()
     try {
       for (let chunk of lib.exportLibrary(library, args.showLibrary, args.tag, args.query, args.sort)) {
-        writer.writeText(chunk, renderFile)
+        p = p.then(writer.writeText(chunk, renderFile))
       }
     }
     catch (err) {
       logger.error('library export failed. %s %o', err, err)
     }
+    await p
 
     renderFile.close()
     console.log('view library at %s', renderPath)
